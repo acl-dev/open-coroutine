18a983fcdf7b68ad5f0c9ea75b24fcfba9630f75
hailang <hailang@webuy.ai>
2022-05-25 22:08:07 +0800

    finish wrap

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,14 +2,12 @@ use std::os::raw::{c_int, c_void};
 use libfiber::fiber::{Fiber, Scheduler};
 use libfiber::libfiber::{ACL_FIBER, acl_fiber_create, acl_fiber_kill, acl_fiber_schedule_stop, acl_fiber_schedule_with, FIBER_EVENT_KERNEL};
 
-///todo 继续往下简化
-unsafe extern "C" fn fiber_main(fiber: *mut ACL_FIBER, arg: *mut c_void) {
-    println!("arg:{}", arg as usize);
-    acl_fiber_kill(fiber);
+fn fiber_main(fiber: &Fiber, arg: Option<*mut c_void>) {
+    println!("arg:{}", arg.unwrap() as usize);
 }
 
 fn main() {
-    Fiber::new(fiber_main, 1 as *mut c_void, 128 * 1024 * 1024);
+    Fiber::new(fiber_main, Some(1 as *mut c_void), 128 * 1024 * 1024);
     let scheduler = Scheduler::new(FIBER_EVENT_KERNEL);
     scheduler.start();
     scheduler.stop();
--- src/fiber.rs
+++ src/fiber.rs
@@ -1,18 +1,36 @@
+use std::mem;
 use std::os::raw::{c_int, c_uint, c_void};
 use crate::libfiber::{ACL_FIBER, acl_fiber_check_timer, acl_fiber_create, acl_fiber_delay, acl_fiber_hook_api, acl_fiber_id, acl_fiber_kill, acl_fiber_killed, acl_fiber_running, acl_fiber_schedule_stop, acl_fiber_schedule_with, acl_fiber_scheduled, acl_fiber_status, acl_fiber_yield, size_t};
 
 pub struct Fiber {
-    fiber: *mut ACL_FIBER,
+    fiber: Option<*mut ACL_FIBER>,
+    ///用户函数
+    function: fn(&Fiber, Option<*mut c_void>),
+    ///用户参数
+    param: Option<*mut c_void>,
 }
 
 impl Fiber {
+    unsafe extern "C" fn fiber_main(_: *mut ACL_FIBER, arg: *mut c_void) {
+        let fiber = mem::transmute::<_, &Fiber>(arg);
+        let function = fiber.function;
+        function(fiber, fiber.param);
+        fiber.exit();
+    }
+
     /// 创建纤程
-    pub fn new(function: unsafe extern "C" fn(fiber: *mut ACL_FIBER, arg: *mut c_void),
-               arg: *mut c_void, size: size_t) -> Self {
-        Fiber {
-            fiber: unsafe {
-                acl_fiber_create(Some(function), arg, size)
-            }
+    pub fn new(function: fn(&Fiber, Option<*mut c_void>),
+               param: Option<*mut c_void>, size: size_t) -> Self {
+        unsafe {
+            let mut fiber = Fiber {
+                fiber: None,
+                function,
+                param,
+            };
+            let native_fiber = acl_fiber_create(Some(Fiber::fiber_main),
+                                                &mut fiber as *mut Fiber as *mut c_void, size);
+            fiber.fiber = Some(native_fiber);
+            fiber
         }
     }
 
@@ -33,28 +51,40 @@ impl Fiber {
     ///获取指定纤程的id
     pub fn get_id(&self) -> c_uint {
         unsafe {
-            acl_fiber_id(self.fiber)
+            match self.fiber {
+                Some(fiber) => acl_fiber_id(fiber),
+                None => 0,
+            }
         }
     }
 
     ///获取指定纤程的状态
     pub fn get_status(&self) -> c_int {
         unsafe {
-            acl_fiber_status(self.fiber)
+            match self.fiber {
+                Some(fiber) => acl_fiber_status(fiber),
+                None => 0,
+            }
         }
     }
 
     ///纤程退出
     pub fn exit(&self) {
         unsafe {
-            acl_fiber_kill(self.fiber)
+            match self.fiber {
+                Some(fiber) => acl_fiber_kill(fiber),
+                None => {}
+            }
         }
     }
 
     ///检查指定的纤程是否已经退出
     pub fn is_exited(&self) -> bool {
         unsafe {
-            acl_fiber_killed(self.fiber) > 0
+            match self.fiber {
+                Some(fiber) => acl_fiber_killed(fiber) > 0,
+                None => true,
+            }
         }
     }
 
