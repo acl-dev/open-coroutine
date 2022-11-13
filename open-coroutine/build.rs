9fcaf91fecdbf17fc08fae6acaf33afb66d3f046
hailang <a1936978077@hotmail.com>
2022-11-13 14:00:47 +0800

    [ISSUE #20] try to fix link problem

--- open-coroutine/build.rs
+++ open-coroutine/build.rs
@@ -1,4 +1,13 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
+    //copy dylib to deps
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let deps = out_dir.parent().unwrap().parent().unwrap().parent().unwrap().join("deps");
+    let dylib = env::current_dir().unwrap().join("dylib");
+    std::fs::copy(dylib.join("libhook.dylib"), deps.join("libhook.dylib"))
+        .expect("copy libhook.dylib failed!");
     //link hook dylib
     println!("cargo:rustc-link-lib=dylib=hook");
 }
