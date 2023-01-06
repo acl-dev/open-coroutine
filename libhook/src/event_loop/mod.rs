9f28eb37ad3bd377179a98dee2397cc908b792ad
hailang <a1936978077@hotmail.com>
2023-01-06 23:21:02 +0800

    remove default timeout

--- libhook/src/event_loop/mod.rs
+++ libhook/src/event_loop/mod.rs
@@ -115,9 +115,7 @@ impl<'a> EventLoop<'a> {
     fn wait(&mut self, timeout: Option<Duration>) -> std::io::Result<()> {
         self.scheduler.syscall();
         let mut events = Events::with_capacity(1024);
-        //默认1s超时
-        self.selector
-            .select(&mut events, Some(timeout.unwrap_or(Duration::from_secs(1))))?;
+        self.selector.select(&mut events, timeout)?;
         for event in events.iter() {
             unsafe {
                 self.scheduler.resume(event.token());
