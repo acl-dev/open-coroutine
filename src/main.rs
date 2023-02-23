6ab669ca37490bbf53e5bbe52b47c3797575301d
hailang <a1936978077@hotmail.com>
2023-02-23 14:55:58 +0800

    模拟scheduler

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,29 @@ use std::ops::{Generator, GeneratorState};
 use std::pin::Pin;
 
 fn main() {
-    let mut generator = || {
+    let mut generator1 = || {
         yield 1;
         "foo"
     };
+    let mut generator2 = || {
+        yield 2;
+        "ha"
+    };
 
-    match Pin::new(&mut generator).resume(()) {
+    match Pin::new(&mut generator1).resume(()) {
         GeneratorState::Yielded(1) => {}
         _ => panic!("unexpected return from resume"),
     }
-    match Pin::new(&mut generator).resume(()) {
+    match Pin::new(&mut generator2).resume(()) {
+        GeneratorState::Yielded(2) => {}
+        _ => panic!("unexpected return from resume"),
+    }
+    match Pin::new(&mut generator1).resume(()) {
         GeneratorState::Complete("foo") => {}
+        _ => panic!("unexpected return from resume"),
+    }
+    match Pin::new(&mut generator2).resume(()) {
+        GeneratorState::Complete("ha") => {}
         _ => panic!("unexpected return from resume"),
     }
 }
