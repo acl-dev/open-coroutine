7db44bdbe3b5b76b2feb540e44af0e6afa426120
hailang <a1936978077@hotmail.com>
2023-03-16 21:49:25 +0800

    add sigsetjmp

--- src/set_jump.rs
+++ src/set_jump.rs
@@ -12,32 +12,55 @@ const _JBLEN: usize = 10 + 16 + 2;
 
 pub type JmpBuf = [libc::c_int; _JBLEN];
 
-/// todo 用汇编实现
+pub type SigJmpBuf = [libc::c_int; _JBLEN + 1];
+
+// todo 用汇编实现
 extern "C" {
 
     pub fn setjmp(env: *mut JmpBuf) -> libc::c_int;
 
     pub fn longjmp(env: *mut JmpBuf, arg: libc::c_int);
+
+    pub fn sigsetjmp(env: *mut SigJmpBuf, save_mask: libc::c_int) -> libc::c_int;
+
+    pub fn siglongjmp(env: *mut SigJmpBuf, arg: libc::c_int);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    unsafe fn func(mut buf: JmpBuf) {
-        println!("func");
-        longjmp(&mut buf, 1);
-        println!("you will never see this because of longjmp");
-    }
-
     #[test]
     fn test() {
+        unsafe fn func(mut buf: JmpBuf) {
+            println!("call longjmp");
+            longjmp(&mut buf, 1);
+            println!("you will never see this because of longjmp");
+        }
         unsafe {
             let mut buf: JmpBuf = std::mem::zeroed();
             if setjmp(&mut buf) != 0 {
-                println!("back to main");
+                println!("setjmp back to main");
             } else {
-                println!("first time through");
+                println!("setjmp first time through");
+                func(buf);
+            }
+        }
+    }
+
+    #[test]
+    fn test_sig() {
+        unsafe fn func(mut buf: SigJmpBuf) {
+            println!("call siglongjmp");
+            siglongjmp(&mut buf, 1);
+            println!("you will never see this because of siglongjmp");
+        }
+        unsafe {
+            let mut buf: SigJmpBuf = std::mem::zeroed();
+            if sigsetjmp(&mut buf, 1) != 0 {
+                println!("sigsetjmp back to main");
+            } else {
+                println!("sigsetjmp first time through");
                 func(buf);
             }
         }
