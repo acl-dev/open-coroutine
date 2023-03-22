1be95ee11a6ec477e148dc3b05026e1dcec4b17a
hailang <a1936978077@hotmail.com>
2023-03-22 13:52:20 +0800

    update CI

--- examples/preemptive.rs
+++ examples/preemptive.rs
@@ -1,86 +1,51 @@
-use open_coroutine::{Scheduler, Yielder};
-use std::ffi::c_void;
-use std::sync::{Arc, Condvar, Mutex};
+use open_coroutine::co;
+use std::os::raw::c_void;
 use std::time::Duration;
 
-fn null() -> &'static mut c_void {
-    unsafe { std::mem::transmute(10usize) }
-}
-
-fn main() -> std::io::Result<()> {
-    static mut TEST_FLAG: bool = true;
-    static mut TEST_FLAG2: bool = true;
-    let pair = Arc::new((Mutex::new(true), Condvar::new()));
-    let pair2 = Arc::clone(&pair);
-    let handler = std::thread::spawn(move || {
-        let mut scheduler = Scheduler::new();
-
-        extern "C" fn f1(
-            _yielder: &Yielder<&'static mut c_void, (), &'static mut c_void>,
-            _input: &'static mut c_void,
-        ) -> &'static mut c_void {
-            println!("coroutine1 launched");
-            while unsafe { TEST_FLAG } {
+#[open_coroutine::main]
+fn main() {
+    static mut EXAMPLE_FLAG1: bool = true;
+    static mut EXAMPLE_FLAG2: bool = true;
+    let handle = co(
+        |_yielder, input: Option<&'static mut i32>| {
+            println!("[coroutine1] launched");
+            while unsafe { EXAMPLE_FLAG1 } {
                 println!("loop1");
                 std::thread::sleep(Duration::from_millis(10));
             }
             println!("loop1 end");
-            null()
-        }
-        scheduler.submit(f1, null(), 4096).expect("submit failed !");
-
-        extern "C" fn f2(
-            _yielder: &Yielder<&'static mut c_void, (), &'static mut c_void>,
-            _input: &'static mut c_void,
-        ) -> &'static mut c_void {
-            println!("coroutine2 launched");
-            while unsafe { TEST_FLAG2 } {
+            input
+        },
+        Some(Box::leak(Box::new(1))),
+        4096,
+    );
+    co(
+        |_yielder, input: Option<&'static mut i32>| {
+            println!("[coroutine2] launched");
+            while unsafe { EXAMPLE_FLAG2 } {
                 println!("loop2");
                 std::thread::sleep(Duration::from_millis(10));
             }
             println!("loop2 end");
-            unsafe { TEST_FLAG = false };
-            null()
-        }
-        scheduler.submit(f2, null(), 4096).expect("submit failed !");
-
-        extern "C" fn f3(
-            _yielder: &Yielder<&'static mut c_void, (), &'static mut c_void>,
-            _input: &'static mut c_void,
-        ) -> &'static mut c_void {
-            println!("coroutine3 launched");
-            unsafe { TEST_FLAG2 = false };
-            null()
-        }
-        scheduler.submit(f3, null(), 4096).expect("submit failed !");
-        scheduler.try_schedule().expect("try_schedule failed !");
-
-        let (lock, cvar) = &*pair2;
-        let mut pending = lock.lock().unwrap();
-        *pending = false;
-        // notify the condvar that the value has changed.
-        cvar.notify_one();
-    });
-
-    // wait for the thread to start up
-    let (lock, cvar) = &*pair;
-    let result = cvar
-        .wait_timeout_while(
-            lock.lock().unwrap(),
-            Duration::from_millis(3000),
-            |&mut pending| pending,
-        )
-        .unwrap();
-    if result.1.timed_out() {
-        Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "preemptive schedule failed",
-        ))
-    } else {
-        unsafe {
-            handler.join().unwrap();
-            assert!(!TEST_FLAG);
-        }
-        Ok(())
+            unsafe { EXAMPLE_FLAG1 = false };
+            input
+        },
+        Some(Box::leak(Box::new(1))),
+        4096,
+    );
+    co(
+        |_yielder, input: Option<&'static mut c_void>| {
+            println!("[coroutine3] launched");
+            unsafe { EXAMPLE_FLAG2 = false };
+            input
+        },
+        None,
+        4096,
+    );
+    let result = handle.join().unwrap().unwrap() as *mut c_void as *mut i32;
+    unsafe {
+        assert_eq!(std::ptr::read_unaligned(result), 1);
+        assert!(!EXAMPLE_FLAG1);
     }
+    println!("preemptive schedule finished successfully!");
 }
