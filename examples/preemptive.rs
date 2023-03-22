71c824a350b2af5ecc2a1986b1cd661bed53054d
hailang <a1936978077@hotmail.com>
2023-03-22 20:48:25 +0800

    rollback example

--- examples/preemptive.rs
+++ examples/preemptive.rs
@@ -4,30 +4,16 @@ use std::time::Duration;
 
 #[open_coroutine::main]
 fn main() {
-    static mut EXAMPLE_FLAG1: bool = true;
-    static mut EXAMPLE_FLAG2: bool = true;
+    static mut EXAMPLE_FLAG: bool = true;
     let handle = co(
         |_yielder, input: Option<&'static mut i32>| {
             println!("[coroutine1] launched");
-            while unsafe { EXAMPLE_FLAG1 } {
-                println!("loop1");
-                std::thread::sleep(Duration::from_millis(10));
+            unsafe {
+                while EXAMPLE_FLAG {
+                    println!("loop");
+                    std::thread::sleep(Duration::from_millis(10));
+                }
             }
-            println!("loop1 end");
-            input
-        },
-        Some(Box::leak(Box::new(1))),
-        4096,
-    );
-    co(
-        |_yielder, input: Option<&'static mut i32>| {
-            println!("[coroutine2] launched");
-            while unsafe { EXAMPLE_FLAG2 } {
-                println!("loop2");
-                std::thread::sleep(Duration::from_millis(10));
-            }
-            println!("loop2 end");
-            unsafe { EXAMPLE_FLAG1 = false };
             input
         },
         Some(Box::leak(Box::new(1))),
@@ -35,8 +21,10 @@ fn main() {
     );
     co(
         |_yielder, input: Option<&'static mut c_void>| {
-            println!("[coroutine3] launched");
-            unsafe { EXAMPLE_FLAG2 = false };
+            println!("[coroutine2] launched");
+            unsafe {
+                EXAMPLE_FLAG = false;
+            }
             input
         },
         None,
@@ -45,7 +33,7 @@ fn main() {
     let result = handle.join().unwrap().unwrap() as *mut c_void as *mut i32;
     unsafe {
         assert_eq!(std::ptr::read_unaligned(result), 1);
-        assert!(!EXAMPLE_FLAG1);
+        assert!(!EXAMPLE_FLAG);
     }
     println!("preemptive schedule finished successfully!");
 }
