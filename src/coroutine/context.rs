c65a52d7759301c5218341ed3885f56dc642cda9
hailang <a1936978077@hotmail.com>
2023-03-24 09:45:12 +0800

    support GeneratorState

--- src/coroutine/context.rs
+++ src/coroutine/context.rs
@@ -2,23 +2,47 @@ use crate::coroutine::set_jmp::{longjmp, setjmp, JmpBuf};
 use std::cell::{Cell, RefCell};
 use std::ffi::c_void;
 use std::fmt::{Debug, Formatter};
+use std::marker::PhantomData;
 use std::mem::MaybeUninit;
 use std::rc::Rc;
 
-pub struct Context<R> {
+/// The result of a generator resumption.
+///
+/// This is modeled after the stdlib's nightly-only
+/// [`std::ops::GeneratorState`].
+#[derive(PartialEq, Debug)]
+#[allow(clippy::module_name_repetitions)]
+pub enum GeneratorState<R> {
+    /// The generator suspended.
+    Yielded,
+
+    /// The generator completed with a return value.
+    Complete(R),
+}
+
+pub struct Context<'c, R> {
     f: Rc<Box<dyn FnOnce()>>,
     from: RefCell<JmpBuf>,
     point: RefCell<JmpBuf>,
     started: Cell<bool>,
     finished: Cell<bool>,
     result: RefCell<MaybeUninit<R>>,
+
+    // We want to be covariant over 'c, Yield and Return, and contravariant
+    // over Input.
+    //
+    // Effectively this means that we can pass a
+    //   ScopedCoroutine<'static, &'a (), &'static (), &'static ()>
+    // to a function that expects a
+    //   ScopedCoroutine<'b, &'static (), &'c (), &'d ()>
+    marker: PhantomData<&'c fn() -> GeneratorState<R>>,
 }
 
 thread_local! {
     static CONTEXT: Box<RefCell<*const c_void>> = Box::new(RefCell::new(std::ptr::null()));
 }
 
-impl<R> Context<R> {
+impl<'c, R> Context<'c, R> {
     pub fn new(f: impl FnOnce(&Context<R>) -> R + 'static) -> Self {
         Context {
             f: Rc::new(Box::new(move || {
@@ -34,6 +58,7 @@ impl<R> Context<R> {
             started: Cell::new(false),
             finished: Cell::new(false),
             result: RefCell::new(MaybeUninit::uninit()),
+            marker: PhantomData,
         }
     }
 
@@ -44,13 +69,13 @@ impl<R> Context<R> {
         });
     }
 
-    pub(crate) fn current<'a>() -> Option<&'a Context<R>> {
+    pub(crate) fn current<'a>() -> Option<&'a Context<'c, R>> {
         CONTEXT.with(|boxed| {
             let ptr = *boxed.borrow_mut();
             if ptr.is_null() {
                 None
             } else {
-                Some(unsafe { &*(ptr.cast::<Context<R>>()) })
+                Some(unsafe { &*(ptr.cast::<Context<'c, R>>()) })
             }
         })
     }
@@ -63,25 +88,23 @@ impl<R> Context<R> {
         self.finished.get()
     }
 
-    pub fn get_result(&self) -> Option<R> {
-        if self.is_finished() {
-            Some(unsafe { self.result.replace(MaybeUninit::uninit()).assume_init() })
-        } else {
-            None
-        }
-    }
-
-    pub fn resume(&self) {
+    pub fn resume(&self) -> GeneratorState<R> {
         unsafe {
             if setjmp(self.from.as_ptr()) == 0 {
                 if self.started.get() {
                     longjmp(self.point.as_ptr(), 1);
-                    unreachable!();
                 } else {
                     self.started.set(true);
                     Context::init_current(self);
                     (std::ptr::read_unaligned(self.f.as_ref()))();
                 }
+                unreachable!();
+            }
+            if self.is_finished() {
+                let r = self.result.replace(MaybeUninit::uninit()).assume_init();
+                GeneratorState::Complete(r)
+            } else {
+                GeneratorState::Yielded
             }
         }
     }
@@ -96,7 +119,7 @@ impl<R> Context<R> {
     }
 }
 
-impl<R> Debug for Context<R> {
+impl<R> Debug for Context<'_, R> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Suspender")
             .field("from", &self.from)
@@ -120,10 +143,8 @@ mod tests {
             println!("third time through");
             1
         });
-        context.resume();
-        context.resume();
-        assert_eq!(context.get_result(), None);
-        context.resume();
-        assert_eq!(context.get_result(), Some(1));
+        assert_eq!(GeneratorState::Yielded, context.resume());
+        assert_eq!(GeneratorState::Yielded, context.resume());
+        assert_eq!(GeneratorState::Complete(1), context.resume());
     }
 }
