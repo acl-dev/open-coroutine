bd2d4609e60ac418d9246689e6d64f59f78b68b8
hailang <a1936978077@hotmail.com>
2023-04-07 21:12:44 +0800

    try pass macos CI

--- src/coroutine/mod.rs
+++ src/coroutine/mod.rs
@@ -79,7 +79,7 @@ macro_rules! co {
         $crate::coroutine::Coroutine::new(
             Box::from(uuid::Uuid::new_v4().to_string()),
             $f,
-            $crate::coroutine::page_size() * 32,
+            $crate::coroutine::page_size() * 256,
         )
         .expect("create coroutine failed !")
     };
@@ -88,8 +88,12 @@ macro_rules! co {
             .expect("create coroutine failed !")
     };
     ($name:literal, $f:expr $(,)?) => {
-        $crate::coroutine::Coroutine::new(Box::from($name), $f, $crate::coroutine::page_size() * 32)
-            .expect("create coroutine failed !")
+        $crate::coroutine::Coroutine::new(
+            Box::from($name),
+            $f,
+            $crate::coroutine::page_size() * 256,
+        )
+        .expect("create coroutine failed !")
     };
 }
 
