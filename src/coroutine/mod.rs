687b08ac2a0126b12e371ba0a7016a1f4febfd3c
hailang <a1936978077@hotmail.com>
2023-04-07 21:39:48 +0800

    try to find min_stack_size

--- src/coroutine/mod.rs
+++ src/coroutine/mod.rs
@@ -79,7 +79,7 @@ macro_rules! co {
         $crate::coroutine::Coroutine::new(
             Box::from(uuid::Uuid::new_v4().to_string()),
             $f,
-            $crate::coroutine::page_size() * 16,
+            $crate::coroutine::page_size() * 8,
         )
         .expect("create coroutine failed !")
     };
@@ -88,7 +88,7 @@ macro_rules! co {
             .expect("create coroutine failed !")
     };
     ($name:literal, $f:expr $(,)?) => {
-        $crate::coroutine::Coroutine::new(Box::from($name), $f, $crate::coroutine::page_size() * 16)
+        $crate::coroutine::Coroutine::new(Box::from($name), $f, $crate::coroutine::page_size() * 8)
             .expect("create coroutine failed !")
     };
 }
