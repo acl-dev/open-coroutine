62c4dcf853378a272af74b857d16e8dd5be3e460
hailang <a1936978077@hotmail.com>
2023-04-08 11:41:03 +0800

    update lib

--- open-coroutine-core/examples/complex_preemptive.rs
+++ open-coroutine-core/examples/complex_preemptive.rs
@@ -22,7 +22,7 @@ fn main() -> std::io::Result<()> {
             println!("coroutine1 launched");
             while unsafe { COMPLEX_TEST_FLAG } {
                 println!("loop1");
-                std::thread::sleep(Duration::from_millis(10));
+                let _ = unsafe { libc::usleep(10_000) };
             }
             println!("loop1 end");
             null()
@@ -36,7 +36,7 @@ fn main() -> std::io::Result<()> {
             println!("coroutine2 launched");
             while unsafe { COMPLEX_TEST_FLAG2 } {
                 println!("loop2");
-                std::thread::sleep(Duration::from_millis(10));
+                let _ = unsafe { libc::usleep(10_000) };
             }
             println!("loop2 end");
             unsafe { COMPLEX_TEST_FLAG = false };
