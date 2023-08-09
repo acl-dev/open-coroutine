6e48becee05f3d2fbab164c243fcb21c462af130
hailang <a1936978077@hotmail.com>
2023-08-09 22:29:14 +0800

    fix build.rs in windows

--- open-coroutine/build.rs
+++ open-coroutine/build.rs
@@ -31,6 +31,24 @@ fn main() {
             std::fs::rename(path, deps.join("libopen_coroutine_hooks.dylib"))
                 .expect("rename to libopen_coroutine_hooks.dylib failed!");
         }
+    } else if cfg!(target_os = "windows") {
+        let dll_pattern = pattern.clone() + "/open_coroutine_hooks*.dll";
+        for path in glob::glob(&dll_pattern)
+            .expect("Failed to read glob pattern")
+            .flatten()
+        {
+            std::fs::rename(path, deps.join("open_coroutine_hooks.dll"))
+                .expect("rename to open_coroutine_hooks.dll failed!");
+        }
+
+        let lib_pattern = pattern + "/open_coroutine_hooks*.lib";
+        for path in glob::glob(&lib_pattern)
+            .expect("Failed to read glob pattern")
+            .flatten()
+        {
+            std::fs::rename(path, deps.join("open_coroutine_hooks.lib"))
+                .expect("rename to open_coroutine_hooks.lib failed!");
+        }
     } else {
         panic!("unsupported platform");
     }
