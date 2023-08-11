190336cf17e6bf83f6dd947a8d5afb3d0c69bb69
hailang <a1936978077@hotmail.com>
2023-08-11 13:46:39 +0800

    use Edge trigger

--- open-coroutine-core/src/event_loop/selector.rs
+++ open-coroutine-core/src/event_loop/selector.rs
@@ -1,5 +1,5 @@
 use once_cell::sync::Lazy;
-use polling::{Event, Poller};
+use polling::{Event, PollMode, Poller};
 use std::collections::HashMap;
 use std::time::Duration;
 
@@ -25,15 +25,27 @@ impl Selector {
         self.0.wait(events, timeout)
     }
 
+    fn get_mode(&self) {
+        if self.0.supports_edge() {
+            PollMode::Edge
+        } else {
+            PollMode::Level
+        }
+    }
+
     pub fn register(
         &self,
         source: libc::c_int,
         token: usize,
         interests: Event,
     ) -> std::io::Result<()> {
-        self.0.add(source, interests).map(|_| {
-            _ = unsafe { TOKEN_FD.insert(token, source) };
-        })
+        #[cfg(windows)]
+        let source = std::os::windows::io::RawSocket::from(source);
+        self.0
+            .add_with_mode(source, interests, self.get_mode())
+            .map(|_| {
+                _ = unsafe { TOKEN_FD.insert(token, source) };
+            })
     }
 
     pub fn reregister(
@@ -42,12 +54,18 @@ impl Selector {
         token: usize,
         interests: Event,
     ) -> std::io::Result<()> {
-        self.0.modify(source, interests).map(|_| {
-            _ = unsafe { TOKEN_FD.insert(token, source) };
-        })
+        #[cfg(windows)]
+        let source = std::os::windows::io::RawSocket::from(source);
+        self.0
+            .modify_with_mode(source, interests, self.get_mode())
+            .map(|_| {
+                _ = unsafe { TOKEN_FD.insert(token, source) };
+            })
     }
 
     pub fn deregister(&self, source: libc::c_int, token: usize) -> std::io::Result<()> {
+        #[cfg(windows)]
+        let source = std::os::windows::io::RawSocket::from(source);
         self.0.delete(source).map(|_| {
             _ = unsafe { TOKEN_FD.remove(&token) };
         })
