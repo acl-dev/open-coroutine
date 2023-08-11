0eccd7768074d4cbb92f0f098778dd535f297799
hailang <a1936978077@hotmail.com>
2023-08-11 13:49:50 +0800

    use polling

--- open-coroutine-core/src/event_loop/event/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Readiness event types and utilities.
-
-#[allow(clippy::module_inception)]
-mod event;
-mod events;
-
-pub use self::event::Event;
-pub use self::events::{Events, Iter};
---
+++ open-coroutine-core/src/event_loop/selector.rs
@@ -0,0 +1,73 @@
+use once_cell::sync::Lazy;
+use polling::{Event, PollMode, Poller};
+use std::collections::HashMap;
+use std::time::Duration;
+
+static mut TOKEN_FD: Lazy<HashMap<usize, libc::c_int>> = Lazy::new(HashMap::new);
+
+#[derive(Debug)]
+pub struct Selector(Poller);
+
+impl Selector {
+    pub fn new() -> std::io::Result<Selector> {
+        Ok(Selector(Poller::new()?))
+    }
+
+    pub fn fd(token: usize) -> libc::c_int {
+        unsafe { TOKEN_FD.remove(&token).unwrap_or(0) }
+    }
+
+    pub fn select(
+        &self,
+        events: &mut Vec<Event>,
+        timeout: Option<Duration>,
+    ) -> std::io::Result<usize> {
+        self.0.wait(events, timeout)
+    }
+
+    fn get_mode(&self) -> PollMode {
+        if self.0.supports_edge() {
+            PollMode::Edge
+        } else {
+            PollMode::Level
+        }
+    }
+
+    pub fn register(
+        &self,
+        source: libc::c_int,
+        token: usize,
+        interests: Event,
+    ) -> std::io::Result<()> {
+        #[cfg(windows)]
+        let source = std::os::windows::io::RawSocket::from(source);
+        self.0
+            .add_with_mode(source, interests, self.get_mode())
+            .map(|_| {
+                _ = unsafe { TOKEN_FD.insert(token, source) };
+            })
+    }
+
+    pub fn reregister(
+        &self,
+        source: libc::c_int,
+        token: usize,
+        interests: Event,
+    ) -> std::io::Result<()> {
+        #[cfg(windows)]
+        let source = std::os::windows::io::RawSocket::from(source);
+        self.0
+            .modify_with_mode(source, interests, self.get_mode())
+            .map(|_| {
+                _ = unsafe { TOKEN_FD.insert(token, source) };
+            })
+    }
+
+    pub fn deregister(&self, source: libc::c_int, token: usize) -> std::io::Result<()> {
+        #[cfg(windows)]
+        let source = std::os::windows::io::RawSocket::from(source);
+        self.0.delete(source).map(|_| {
+            _ = unsafe { TOKEN_FD.remove(&token) };
+        })
+    }
+}
