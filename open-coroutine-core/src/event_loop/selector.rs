ebf590cffcf0ce38a4393fb126f5e9ff7058a5dc
hailang <a1936978077@hotmail.com>
2023-08-11 14:15:30 +0800

    try fix windows compile

--- open-coroutine-core/src/event_loop/selector.rs
+++ open-coroutine-core/src/event_loop/selector.rs
@@ -33,39 +33,49 @@ impl Selector {
         }
     }
 
-    pub fn register(
-        &self,
-        source: libc::c_int,
-        token: usize,
-        interests: Event,
-    ) -> std::io::Result<()> {
-        #[cfg(windows)]
-        let source = std::os::windows::io::RawSocket::from(source);
+    pub fn register(&self, fd: libc::c_int, token: usize, interests: Event) -> std::io::Result<()> {
+        cfg_if::cfg_if! {
+            if #[cfg(windows)] {
+                let source = std::os::windows::io::RawSocket::from(fd as u64);
+            } else {
+                let source = fd;
+            }
+        }
         self.0
             .add_with_mode(source, interests, self.get_mode())
             .map(|_| {
-                _ = unsafe { TOKEN_FD.insert(token, source) };
+                _ = unsafe { TOKEN_FD.insert(token, fd) };
             })
     }
 
     pub fn reregister(
         &self,
-        source: libc::c_int,
+        fd: libc::c_int,
         token: usize,
         interests: Event,
     ) -> std::io::Result<()> {
-        #[cfg(windows)]
-        let source = std::os::windows::io::RawSocket::from(source);
+        cfg_if::cfg_if! {
+            if #[cfg(windows)] {
+                let source = std::os::windows::io::RawSocket::from(fd as u64);
+            } else {
+                let source = fd;
+            }
+        }
         self.0
             .modify_with_mode(source, interests, self.get_mode())
             .map(|_| {
-                _ = unsafe { TOKEN_FD.insert(token, source) };
+                _ = unsafe { TOKEN_FD.insert(token, fd) };
             })
     }
 
-    pub fn deregister(&self, source: libc::c_int, token: usize) -> std::io::Result<()> {
-        #[cfg(windows)]
-        let source = std::os::windows::io::RawSocket::from(source);
+    pub fn deregister(&self, fd: libc::c_int, token: usize) -> std::io::Result<()> {
+        cfg_if::cfg_if! {
+            if #[cfg(windows)] {
+                let source = std::os::windows::io::RawSocket::from(fd as u64);
+            } else {
+                let source = fd;
+            }
+        }
         self.0.delete(source).map(|_| {
             _ = unsafe { TOKEN_FD.remove(&token) };
         })
