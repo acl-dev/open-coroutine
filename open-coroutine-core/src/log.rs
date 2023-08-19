b95fba2b3b89a00a1eb3317f2655d3216b08b22b
hailang <a1936978077@hotmail.com>
2023-08-19 18:50:43 +0800

    fix linux CI

--- open-coroutine-core/src/log.rs
+++ open-coroutine-core/src/log.rs
@@ -9,14 +9,17 @@ macro_rules! info {
         cfg_if::cfg_if! {
             if #[cfg(feature = "logs")] {
                 $crate::log::LOG.call_once(|| {
+                    let mut builder = simplelog::ConfigBuilder::new();
+                    let result = builder.set_time_format_rfc2822().set_time_offset_to_local();
+                    let config = if let Ok(builder) = result {
+                        builder
+                    } else {
+                        result.unwrap_err()
+                    }
+                    .build();
                     _ = simplelog::CombinedLogger::init(vec![simplelog::TermLogger::new(
                         log::LevelFilter::Info,
-                        simplelog::ConfigBuilder::new()
-                            .set_time_format_rfc2822()
-                            .set_time_offset_to_local()
-                            .map(simplelog::ConfigBuilder::build)
-                            .map_err(simplelog::ConfigBuilder::build)
-                            .unwrap(),
+                        config,
                         simplelog::TerminalMode::Mixed,
                         simplelog::ColorChoice::Auto,
                     )]);
@@ -31,14 +34,17 @@ macro_rules! info {
         cfg_if::cfg_if! {
             if #[cfg(feature = "logs")] {
                 $crate::log::LOG.call_once(|| {
+                    let mut builder = simplelog::ConfigBuilder::new();
+                    let result = builder.set_time_format_rfc2822().set_time_offset_to_local();
+                    let config = if let Ok(builder) = result {
+                        builder
+                    } else {
+                        result.unwrap_err()
+                    }
+                    .build();
                     _ = simplelog::CombinedLogger::init(vec![simplelog::TermLogger::new(
                         log::LevelFilter::Info,
-                        simplelog::ConfigBuilder::new()
-                            .set_time_format_rfc2822()
-                            .set_time_offset_to_local()
-                            .map(simplelog::ConfigBuilder::build)
-                            .map_err(simplelog::ConfigBuilder::build)
-                            .unwrap(),
+                        config,
                         simplelog::TerminalMode::Mixed,
                         simplelog::ColorChoice::Auto,
                     )]);
