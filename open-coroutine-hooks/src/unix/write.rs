5cc5ad4949e83537c7caefafeded9944e9bda76e
hailang <a1936978077@hotmail.com>
2023-08-22 08:13:19 +0800

    hook sendmsg and recvfrom

--- open-coroutine-hooks/src/unix/write.rs
+++ open-coroutine-hooks/src/unix/write.rs
@@ -1,4 +1,4 @@
-use libc::{c_int, iovec, off_t, size_t, sockaddr, socklen_t, ssize_t};
+use libc::{c_int, iovec, msghdr, off_t, size_t, sockaddr, socklen_t, ssize_t};
 use once_cell::sync::Lazy;
 use std::ffi::c_void;
 
@@ -65,5 +65,78 @@ pub extern "C" fn pwritev(fd: c_int, iov: *const iovec, iovcnt: c_int, offset: o
     open_coroutine_core::unbreakable!(
         impl_expected_batch_write_hook!((Lazy::force(&PWRITEV))(fd, iov, iovcnt, offset)),
         "pwritev"
+    )
+}
+
+static SENDMSG: Lazy<extern "C" fn(c_int, *const msghdr, c_int) -> ssize_t> = init_hook!("sendmsg");
+
+#[allow(clippy::cast_possible_wrap, clippy::cast_sign_loss)]
+#[no_mangle]
+pub extern "C" fn sendmsg(fd: c_int, msg: *const msghdr, flags: c_int) -> ssize_t {
+    open_coroutine_core::unbreakable!(
+        {
+            let blocking = crate::unix::is_blocking(fd);
+            if blocking {
+                crate::unix::set_non_blocking(fd);
+            }
+            let msghdr = unsafe { *msg };
+            let vec = unsafe {
+                Vec::from_raw_parts(
+                    msghdr.msg_iov,
+                    msghdr.msg_iovlen as usize,
+                    msghdr.msg_iovlen as usize,
+                )
+            };
+            let mut total_sent = 0;
+            for iovec in vec {
+                let length = iovec.iov_len;
+                let mut sent = 0;
+                let mut r = 0;
+                while sent < length {
+                    let mut inner_iovec = iovec {
+                        iov_base: (iovec.iov_base as usize + sent) as *mut c_void,
+                        iov_len: length - sent,
+                    };
+                    let inner_msghdr = msghdr {
+                        msg_name: msghdr.msg_name,
+                        msg_namelen: msghdr.msg_namelen,
+                        msg_iov: &mut inner_iovec,
+                        msg_iovlen: 1,
+                        msg_control: msghdr.msg_control,
+                        msg_controllen: msghdr.msg_controllen,
+                        msg_flags: msghdr.msg_flags,
+                    };
+                    r = (Lazy::force(&SENDMSG))(fd, &inner_msghdr, flags);
+                    if r != -1 {
+                        crate::unix::reset_errno();
+                        sent += r as size_t;
+                        if sent >= length {
+                            r = sent as ssize_t;
+                            break;
+                        }
+                    }
+                    let error_kind = std::io::Error::last_os_error().kind();
+                    if error_kind == std::io::ErrorKind::WouldBlock {
+                        //wait write event
+                        if open_coroutine_core::event_loop::EventLoops::wait_write_event(
+                            fd,
+                            Some(std::time::Duration::from_millis(10)),
+                        )
+                        .is_err()
+                        {
+                            return -1;
+                        }
+                    } else if error_kind != std::io::ErrorKind::Interrupted {
+                        return -1;
+                    }
+                }
+                total_sent += r;
+            }
+            if blocking {
+                crate::unix::set_blocking(fd);
+            }
+            total_sent
+        },
+        "sendmsg"
     )
 }
