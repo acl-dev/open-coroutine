298ab030cfa6644f3074b3a441f93494d4457d4e
hailang <a1936978077@hotmail.com>
2023-08-22 19:28:45 +0800

    fix compile

--- open-coroutine-hooks/src/unix/write.rs
+++ open-coroutine-hooks/src/unix/write.rs
@@ -124,23 +124,23 @@ pub extern "C" fn sendmsg(fd: c_int, msg: *const msghdr, flags: c_int) -> ssize_
                     iov_base: (vec[0].iov_base as usize + current_sent_offset) as *mut c_void,
                     iov_len: vec[0].iov_len - current_sent_offset,
                 };
-
+                cfg_if::cfg_if! {
+                    if #[cfg(any(
+                        target_os = "linux",
+                        target_os = "l4re",
+                        target_os = "android",
+                        target_os = "emscripten"
+                    ))] {
+                        let len = vec.len();
+                    } else {
+                        let len = c_int::try_from(vec.len()).unwrap();
+                    }
+                }
                 let new_msg = msghdr {
                     msg_name: msghdr.msg_name,
                     msg_namelen: msghdr.msg_namelen,
                     msg_iov: vec.get_mut(0).unwrap(),
-                    msg_iovlen: cfg_if::cfg_if! {
-                        if #[cfg(any(
-                            target_os = "linux",
-                            target_os = "l4re",
-                            target_os = "android",
-                            target_os = "emscripten"
-                        ))] {
-                            vec.len()
-                        } else {
-                            c_int::try_from(vec.len()).unwrap()
-                        }
-                    },
+                    msg_iovlen: len,
                     msg_control: msghdr.msg_control,
                     msg_controllen: msghdr.msg_controllen,
                     msg_flags: msghdr.msg_flags,
