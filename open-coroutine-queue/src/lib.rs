ee2c00bce2d00c02de56fb710d564dfca4c4485b
hailang <a1936978077@hotmail.com>
2023-11-06 20:02:40 +0800

    refactor queue mod

--- open-coroutine-queue/src/lib.rs
+++ open-coroutine-queue/src/lib.rs
@@ -3,23 +3,22 @@
     // https://doc.rust-lang.org/rustc/lints/listing/allowed-by-default.html
     anonymous_parameters,
     bare_trait_objects,
-    // box_pointers, // use box pointer to allocate on heap
-    // elided_lifetimes_in_paths, // allow anonymous lifetime
+    box_pointers,
+    elided_lifetimes_in_paths,
     missing_copy_implementations,
     missing_debug_implementations,
-    // missing_docs, // TODO: add documents
-    // single_use_lifetimes, // TODO: fix lifetime names only used once
-    // trivial_casts,
+    missing_docs,
+    single_use_lifetimes,
+    trivial_casts,
     trivial_numeric_casts,
-    // unreachable_pub, allow clippy::redundant_pub_crate lint instead
-    // unsafe_code,
+    unreachable_pub,
+    unsafe_code,
     unstable_features,
     unused_extern_crates,
     unused_import_braces,
     unused_qualifications,
     unused_results,
     variant_size_differences,
-
     warnings, // treat all wanings as errors
 
     clippy::all,
@@ -34,16 +33,14 @@
     clippy::implicit_return, // actually omitting the return keyword is idiomatic Rust code
     clippy::module_name_repetitions, // repeation of module name in a struct name is not big deal
     clippy::multiple_crate_versions, // multi-version dependency crates is not able to fix
-    clippy::missing_errors_doc, // TODO: add error docs
-    clippy::missing_panics_doc, // TODO: add panic docs
     clippy::panic_in_result_fn,
     clippy::shadow_same, // Not too much bad
     clippy::shadow_reuse, // Not too much bad
     clippy::exhaustive_enums,
     clippy::exhaustive_structs,
     clippy::indexing_slicing,
+    clippy::wildcard_imports,
     clippy::separated_literal_suffix, // conflicts with clippy::unseparated_literal_suffix
-    clippy::single_char_lifetime_names, // TODO: change lifetime names
 )]
 
 //! Suppose a thread in a work-stealing scheduler is idle and looking for the next task to run. To
@@ -85,6 +82,9 @@
 pub use rand::*;
 pub use work_steal::*;
 
+/// rand impl for work steal queue
+#[allow(missing_docs)]
 pub mod rand;
 
+/// work steal queue impl
 pub mod work_steal;
--- open-coroutine-queue/src/work_steal.rs
+++ open-coroutine-queue/src/work_steal.rs
@@ -1,9 +1,11 @@
 use crate::rand::{FastRand, RngSeedGenerator};
 use crossbeam_deque::{Injector, Steal};
 use st3::fifo::Worker;
+use std::collections::VecDeque;
 use std::fmt::Debug;
 use std::sync::atomic::{AtomicBool, AtomicU32, AtomicUsize, Ordering};
 
+/// Work stealing global queue, shared by multiple threads.
 #[repr(C)]
 #[derive(Debug)]
 pub struct WorkStealQueue<T: Debug> {
@@ -11,7 +13,7 @@ pub struct WorkStealQueue<T: Debug> {
     /// Number of pending tasks in the queue. This helps prevent unnecessary
     /// locking in the hot path.
     len: AtomicUsize,
-    local_queues: Box<[Worker<T>]>,
+    local_queues: VecDeque<Worker<T>>,
     index: AtomicUsize,
     seed_generator: RngSeedGenerator,
 }
@@ -19,7 +21,7 @@ pub struct WorkStealQueue<T: Debug> {
 impl<T: Debug> Drop for WorkStealQueue<T> {
     fn drop(&mut self) {
         if !std::thread::panicking() {
-            for local_queue in &*self.local_queues {
+            for local_queue in &self.local_queues {
                 assert!(local_queue.pop().is_none(), "local queue not empty");
             }
             assert!(self.pop().is_none(), "global queue not empty");
@@ -27,16 +29,27 @@ impl<T: Debug> Drop for WorkStealQueue<T> {
     }
 }
 
-unsafe impl<T: Send + Debug> Send for WorkStealQueue<T> {}
-unsafe impl<T: Send + Debug> Sync for WorkStealQueue<T> {}
-
 impl<T: Debug> WorkStealQueue<T> {
+    /// Get a global `WorkStealQueue` instance.
+    #[allow(unsafe_code, trivial_casts, box_pointers)]
+    pub fn get_instance<'s>() -> &'s WorkStealQueue<T> {
+        static INSTANCE: AtomicUsize = AtomicUsize::new(0);
+        let mut ret = INSTANCE.load(Ordering::Relaxed);
+        if ret == 0 {
+            let ptr: &'s mut WorkStealQueue<T> = Box::leak(Box::default());
+            ret = ptr as *mut WorkStealQueue<T> as usize;
+            INSTANCE.store(ret, Ordering::Relaxed);
+        }
+        unsafe { &*(ret as *mut WorkStealQueue<T>) }
+    }
+
+    /// Create a new `WorkStealQueue` instance.
     #[must_use]
-    pub fn new(local_queues: usize, local_capacity: usize) -> Self {
+    pub fn new(local_queues_size: usize, local_capacity: usize) -> Self {
         WorkStealQueue {
             shared_queue: Injector::new(),
             len: AtomicUsize::new(0),
-            local_queues: (0..local_queues)
+            local_queues: (0..local_queues_size)
                 .map(|_| Worker::new(local_capacity))
                 .collect(),
             index: AtomicUsize::new(0),
@@ -44,20 +57,24 @@ impl<T: Debug> WorkStealQueue<T> {
         }
     }
 
+    /// Returns `true` if the global queue is empty.
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    /// Get the size of the global queue.
     pub fn len(&self) -> usize {
         self.len.load(Ordering::Acquire)
     }
 
+    /// Push an element to the global queue.
     pub fn push(&self, item: T) {
         self.shared_queue.push(item);
         //add count
         self.len.store(self.len() + 1, Ordering::Release);
     }
 
+    /// Pop an element from the global queue.
     pub fn pop(&self) -> Option<T> {
         // Fast path, if len == 0, then there are no values
         if self.is_empty() {
@@ -76,7 +93,11 @@ impl<T: Debug> WorkStealQueue<T> {
         }
     }
 
-    pub fn local_queue(&self) -> LocalQueue<T> {
+    /// Get a local queue, this method should be called up to `local_queue_size` times.
+    ///
+    /// # Panics
+    /// should never happens
+    pub fn local_queue(&self) -> LocalQueue<'_, T> {
         let mut index = self.index.fetch_add(1, Ordering::Relaxed);
         if index == usize::MAX {
             self.index.store(0, Ordering::Relaxed);
@@ -96,6 +117,7 @@ impl<T: Debug> Default for WorkStealQueue<T> {
     }
 }
 
+/// The work stealing local queue, exclusive to thread.
 #[repr(C)]
 #[derive(Debug)]
 pub struct LocalQueue<'l, T: Debug> {
@@ -108,6 +130,12 @@ pub struct LocalQueue<'l, T: Debug> {
     rand: FastRand,
 }
 
+impl<T: Debug> Default for LocalQueue<'_, T> {
+    fn default() -> Self {
+        WorkStealQueue::get_instance().local_queue()
+    }
+}
+
 impl<T: Debug> Drop for LocalQueue<'_, T> {
     fn drop(&mut self) {
         if !std::thread::panicking() {
@@ -116,11 +144,8 @@ impl<T: Debug> Drop for LocalQueue<'_, T> {
     }
 }
 
-unsafe impl<T: Send + Debug> Send for LocalQueue<'_, T> {}
-unsafe impl<T: Send + Debug> Sync for LocalQueue<'_, T> {}
-
 impl<'l, T: Debug> LocalQueue<'l, T> {
-    pub(crate) fn new(shared: &'l WorkStealQueue<T>, queue: &'l Worker<T>, rand: FastRand) -> Self {
+    fn new(shared: &'l WorkStealQueue<T>, queue: &'l Worker<T>, rand: FastRand) -> Self {
         LocalQueue {
             tick: AtomicU32::new(0),
             shared,
@@ -130,14 +155,36 @@ impl<'l, T: Debug> LocalQueue<'l, T> {
         }
     }
 
+    /// Returns `true` if the local queue is empty.
     pub fn is_empty(&self) -> bool {
         self.queue.is_empty()
     }
 
+    /// Returns `true` if the local queue is full.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use open_coroutine_queue::WorkStealQueue;
+    ///
+    /// let queue = WorkStealQueue::new(1, 2);
+    /// let local = queue.local_queue();
+    /// assert!(local.is_empty());
+    /// for i in 0..2 {
+    ///     local.push_back(i);
+    /// }
+    /// assert!(local.is_full());
+    /// assert_eq!(local.pop_front(), Some(0));
+    /// assert_eq!(local.len(), 1);
+    /// assert_eq!(local.pop_front(), Some(1));
+    /// assert_eq!(local.pop_front(), None);
+    /// assert!(local.is_empty());
+    /// ```
     pub fn is_full(&self) -> bool {
-        self.queue.capacity() == self.len()
+        self.queue.spare_capacity() == 0
     }
 
+    /// Returns the number of elements in the queue.
     pub fn len(&self) -> usize {
         self.queue.capacity() - self.queue.spare_capacity()
     }
@@ -254,31 +301,32 @@ impl<'l, T: Debug> LocalQueue<'l, T> {
             let start = self.rand.fastrand_n(num as u32) as usize;
             for i in 0..num {
                 let i = (start + i) % num;
-                let another: &Worker<T> = local_queues.get(i).expect("get local queue failed!");
-                if std::ptr::eq(&another, &self.queue) {
-                    //不能偷自己
-                    continue;
-                }
-                if another.is_empty() {
-                    //其他队列为空
-                    continue;
-                }
-                if self.queue.spare_capacity() == 0 {
-                    //本地队列已满
-                    continue;
-                }
-                if another
-                    .stealer()
-                    .steal(self.queue, |n| {
-                        //可偷取的最大长度与本地队列空闲长度做比较
-                        n.min(self.queue.spare_capacity())
-                            //与其他队列当前长度的一半做比较
-                            .min(((another.capacity() - another.spare_capacity()) + 1) / 2)
-                    })
-                    .is_ok()
-                {
-                    self.release_lock();
-                    return self.queue.pop();
+                if let Some(another) = local_queues.get(i) {
+                    if std::ptr::eq(&another, &self.queue) {
+                        //不能偷自己
+                        continue;
+                    }
+                    if another.is_empty() {
+                        //其他队列为空
+                        continue;
+                    }
+                    if self.queue.spare_capacity() == 0 {
+                        //本地队列已满
+                        continue;
+                    }
+                    if another
+                        .stealer()
+                        .steal(self.queue, |n| {
+                            //可偷取的最大长度与本地队列空闲长度做比较
+                            n.min(self.queue.spare_capacity())
+                                //与其他队列当前长度的一半做比较
+                                .min(((another.capacity() - another.spare_capacity()) + 1) / 2)
+                        })
+                        .is_ok()
+                    {
+                        self.release_lock();
+                        return self.queue.pop();
+                    }
                 }
             }
             self.release_lock();
