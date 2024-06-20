9a1a13ddca76dbe7df6a16e4789706002dea3766
hailang <a1936978077@hotmail.com>
2024-06-20 10:05:17 +0800

    fix monitor bug

--- open-coroutine-core/src/monitor/mod.rs
+++ open-coroutine-core/src/monitor/mod.rs
@@ -11,11 +11,14 @@ use nix::sys::pthread::pthread_kill;
 use nix::sys::signal::{sigaction, SaFlags, SigAction, SigHandler, SigSet, Signal};
 use once_cell::sync::Lazy;
 use open_coroutine_timer::now;
-use std::cell::Cell;
+use std::cell::{Cell, UnsafeCell};
 use std::collections::HashSet;
+use std::io::{Error, ErrorKind};
+use std::mem::MaybeUninit;
 use std::panic::AssertUnwindSafe;
 use std::sync::atomic::Ordering;
 use std::thread::JoinHandle;
+use std::time::Duration;
 
 mod node;
 
@@ -27,20 +30,20 @@ static mut GLOBAL: Lazy<Monitor> = Lazy::new(Monitor::new);
 #[derive(Debug)]
 pub(crate) struct Monitor {
     cpu: usize,
-    notify_queue: HashSet<NotifyNode>,
+    notify_queue: UnsafeCell<HashSet<NotifyNode>>,
     state: Cell<MonitorState>,
-    thread: JoinHandle<()>,
+    thread: UnsafeCell<MaybeUninit<JoinHandle<()>>>,
 }
 
 impl Drop for Monitor {
     fn drop(&mut self) {
         if !std::thread::panicking() {
             assert!(
-                self.notify_queue.is_empty(),
+                self.notify_queue.get_mut().is_empty(),
                 "there are still timer tasks to be carried out !"
             );
             assert!(
-                self.thread.is_finished(),
+                unsafe { self.thread.get_mut().assume_init_mut().is_finished() },
                 "the monitor thread not finished !"
             );
         }
@@ -48,18 +51,6 @@ impl Drop for Monitor {
 }
 
 impl_current_for!(MONITOR, Monitor);
-
-extern "C" fn sigurg_handler(_: libc::c_int) {
-    if let Ok(mut set) = SigSet::thread_get_mask() {
-        //删除对SIGURG信号的屏蔽，使信号处理函数即使在处理中，也可以再次进入信号处理函数
-        set.remove(Signal::SIGURG);
-        set.thread_set_mask()
-            .expect("Failed to remove SIGURG signal mask!");
-        if let Some(suspender) = SchedulableSuspender::current() {
-            suspender.suspend();
-        }
-    }
-}
 
 impl Monitor {
     #[cfg(unix)]
@@ -81,9 +72,10 @@ impl Monitor {
             warn!("pin monitor thread to CPU core-{MONITOR_CPU} failed !");
         }
         let monitor = Monitor::get_instance();
-        while MonitorState::Running == monitor.state.get() || !monitor.notify_queue.is_empty() {
+        let notify_queue = unsafe { &*monitor.notify_queue.get() };
+        while MonitorState::Running == monitor.state.get() || !notify_queue.is_empty() {
             //只遍历，不删除，如果抢占调度失败，会在1ms后不断重试，相当于主动检测
-            for node in &monitor.notify_queue {
+            for node in notify_queue {
                 if now() < node.timestamp() {
                     continue;
                 }
@@ -100,7 +92,7 @@ impl Monitor {
                 if #[cfg(feature = "net")] {
                     //monitor线程不执行协程计算任务，每次循环至少wait 1ms
                     let event_loop = EventLoops::monitor();
-                    _ = event_loop.wait_just(Some(std::time::Duration::from_millis(1)));
+                    _ = event_loop.wait_just(Some(Duration::from_millis(1)));
                     //push tasks to other event-loop
                     while let Some(task) = event_loop.pop() {
                         EventLoops::submit_raw(task);
@@ -111,36 +103,66 @@ impl Monitor {
     }
 
     fn new() -> Self {
-        Monitor::register_handler(sigurg_handler);
         //初始化monitor线程
         Monitor {
             cpu: MONITOR_CPU,
-            notify_queue: HashSet::new(),
+            notify_queue: UnsafeCell::default(),
             state: Cell::new(MonitorState::Created),
-            thread: std::thread::Builder::new()
-                .name("open-coroutine-monitor".to_string())
-                .spawn(|| {
-                    Monitor::init_current(Monitor::get_instance());
-                    #[allow(unused_variables)]
-                    if let Err(e) =
-                        std::panic::catch_unwind(AssertUnwindSafe(Monitor::monitor_thread_main))
-                    {
-                        #[cfg(feature = "logs")]
-                        let message = *e
-                            .downcast_ref::<&'static str>()
-                            .unwrap_or(&"Monitor failed without message");
-                        error!("open-coroutine-monitor exited with error:{}", message);
-                    } else {
-                        warn!("open-coroutine-monitor has exited");
-                    }
-                    Monitor::clean_current();
-                })
-                .expect("failed to spawn monitor thread"),
+            thread: UnsafeCell::new(MaybeUninit::uninit()),
         }
     }
 
-    fn get_instance() -> &'static mut Monitor {
-        unsafe { &mut *std::ptr::addr_of_mut!(GLOBAL) }
+    fn get_instance() -> &'static Monitor {
+        unsafe { &*std::ptr::addr_of!(GLOBAL) }
+    }
+
+    fn start(&self) -> std::io::Result<()> {
+        extern "C" fn sigurg_handler(_: libc::c_int) {
+            if let Ok(mut set) = SigSet::thread_get_mask() {
+                //删除对SIGURG信号的屏蔽，使信号处理函数即使在处理中，也可以再次进入信号处理函数
+                set.remove(Signal::SIGURG);
+                set.thread_set_mask()
+                    .expect("Failed to remove SIGURG signal mask!");
+                if let Some(suspender) = SchedulableSuspender::current() {
+                    suspender.suspend();
+                }
+            }
+        }
+        match self.state.get() {
+            MonitorState::Created => {
+                self.state.set(MonitorState::Running);
+                // install SIGURG signal handler
+                Monitor::register_handler(sigurg_handler);
+                // start the monitor thread
+                let monitor = unsafe { &mut *self.thread.get() };
+                *monitor = MaybeUninit::new(
+                    std::thread::Builder::new()
+                        .name("open-coroutine-monitor".to_string())
+                        .spawn(|| {
+                            Monitor::init_current(Monitor::get_instance());
+                            #[allow(unused_variables)]
+                            if let Err(e) = std::panic::catch_unwind(AssertUnwindSafe(
+                                Monitor::monitor_thread_main,
+                            )) {
+                                #[cfg(feature = "logs")]
+                                let message = *e
+                                    .downcast_ref::<&'static str>()
+                                    .unwrap_or(&"Monitor failed without message");
+                                error!("open-coroutine-monitor exited with error:{}", message);
+                            } else {
+                                warn!("open-coroutine-monitor has exited");
+                            }
+                            Monitor::clean_current();
+                        })?,
+                );
+                Ok(())
+            }
+            MonitorState::Running => Ok(()),
+            MonitorState::Stopping | MonitorState::Stopped => Err(Error::new(
+                ErrorKind::Unsupported,
+                "Restart operation is unsupported !",
+            )),
+        }
     }
 
     #[allow(dead_code)]
@@ -160,14 +182,18 @@ impl Monitor {
         }
     }
 
-    #[allow(clippy::unnecessary_wraps)]
     fn submit(timestamp: u64) -> std::io::Result<NotifyNode> {
+        let instance = Self::get_instance();
+        instance.start()?;
+        let queue = unsafe { &mut *instance.notify_queue.get() };
         let node = NotifyNode::new(timestamp);
-        _ = Self::get_instance().notify_queue.insert(node);
+        _ = queue.insert(node);
         Ok(node)
     }
 
     fn remove(node: &NotifyNode) -> bool {
-        Self::get_instance().notify_queue.remove(node)
+        let instance = Self::get_instance();
+        let queue = unsafe { &mut *instance.notify_queue.get() };
+        queue.remove(node)
     }
 }
